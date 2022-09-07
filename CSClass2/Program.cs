ead13afadc295f8fcde6376c9ffd0ea327b17f14
seohyunz <joseohyun0201@gmail.com>
2022-09-07 09:12:29 +0900

    #10 인터페이스 응용 : StreamWriter

--- CSClass2/Program.cs
+++ CSClass2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using CSClass2;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -113,6 +114,27 @@ IComparable<Child> childAsComparable = c;
 FileStyleUriParser.WriteAllText(@"C:\Temp\test.txt","문자열 메시지를 씁니다");
 Console.WriteLine(FileStyleUriParser.ReadAllText(@"C:\Temp\test.txt"));
 
+//StreamWriter writer = new StreamWriter(@"C:\Temp\test.txt")
+using (StreamWriter writer = new StreamWriter(@"C:\Temp\test.txt"))
+{
+    string line;
+    while((line = BinaryReader.ReadLine()) != null)
+    {
+        Console.WriteLine(File.ReadAllText(@"C:\Temp\test.txt"));
+    }
+
+    writer.WriteLine("안녕하세요");
+    writer.WriteLine("StreamWriter 클래스를 사용해");
+    writer.WriteLine("글자를");
+    writer.WriteLine("여러줄");
+    writer.WriteLine("입력해봅시다");
+    for(int i =0; i<10; i++)
+    {
+        writer.WriteLine("반복문" + i);
+    }
+
+}
+Console.WriteLine(File.ReadAllText(@"C:\Temp\test.txt"));
 
     }
 }
